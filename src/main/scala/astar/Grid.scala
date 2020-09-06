0937a9bd3e589f57d5ef5f5881d65bb0dc3d8dcb
MrMoonshine <mrmoonshine.goo@gmail.com>
2020-09-06 15:48:01 +0200

    experimental version 1

--- src/main/scala/astar/Grid.scala
+++ src/main/scala/astar/Grid.scala
@@ -1,5 +1,7 @@
 package astar
 
+import java.awt.Point
+
 //A grid is a A* data structure
 class Grid(grid_i:Array[Array[Boolean]],diagonal:Boolean = true) {
   val height = grid_i(0).length
@@ -22,6 +24,48 @@ class Grid(grid_i:Array[Array[Boolean]],diagonal:Boolean = true) {
 
     noff
   }
+  private def getValidGridNeighbors(i_i:Int,j_i:Int):Array[Array[Int]] = {
+    var posarr:Array[Array[Int]] = Array()
+    for(n <- neighborOffsets){
+      val i = i_i + n(0)
+      val j = j_i + n(1)
+      if(i < width && i >= 0 && j < width && j >= 0){
+        posarr :+= Array(i,j)
+      }
+    }
+    posarr
+  }
+  private def distance(a_i:Point,b_i:Point): Double ={
+    val deltaX:Double = a_i.x - b_i.x
+    val deltaY:Double = a_i.y - b_i.y
+    //println(s"deltaX: $deltaX ; deltaY $deltaY")
+    Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2))/1000
+  }
+
+  def build(): Array[Node] = {
+    var arr2d:Array[Array[Option[Node]]] = Array()
+    var oarr:Array[Node] = Array()
+    for(i <- 0 until width; j <- 0 until height){
+      arr2d(i)(j) = Some(new Node(new Point(i * 10,j * 10)){
+        name = s"GridNode_${i}_${j}"
+      })
+    }
+
+    for(i <- 0 until width; j <- 0 until height){
+      for(n <- getValidGridNeighbors(i,j)){
+        if(grid_i(n(0))(n(1))){
+          arr2d(i)(j).get.addNeighbor()
+        }else{
+          arr2d(n(0))(n(1)) = None
+        }
+      }
+    }
+
+    for(i <- 0 until width; j <- 0 until height){
+      oarr :+= arr2d(i)(j)
+    }
+    oarr
+  }
 
   override def toString: String = {
     "Grid!!!"
@@ -30,6 +74,7 @@ class Grid(grid_i:Array[Array[Boolean]],diagonal:Boolean = true) {
   if(height < 1 || width < 1){
     throw new Exception("Insufficient data provided")
   }
+
 
 
 }
