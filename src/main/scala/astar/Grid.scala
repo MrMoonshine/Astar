60361d86bb3a73d1e363d7e75f6682a77b01d1a1
MrMoonshine <mrmoonshine.goo@gmail.com>
2020-09-06 14:47:43 +0200

    experimental version 1

--- src/main/scala/astar/Grid.scala
+++ src/main/scala/astar/Grid.scala
@@ -1,8 +1,34 @@
 package astar
 
-class Grid(width_i:Int,height_i:Int) {
-  if(width_i < 1 || height_i < 1){
-    throw new Exception("Invalid grid witdth or height")
+//A grid is a A* data structure
+class Grid(grid_i:Array[Array[Boolean]],diagonal:Boolean = true) {
+  val height = grid_i(0).length
+  val width = grid_i.length
+  val neighborOffsets:Array[Array[Int]] = fetchNeighborOffsets()
+
+  private def fetchNeighborOffsets(): Array[Array[Int]] ={
+    var noff:Array[Array[Int]] = Array()
+    noff :+= Array(-1,0)
+    noff :+= Array(1,0)
+    noff :+= Array(0,1)
+    noff :+= Array(0,-1)
+
+    if(diagonal){
+      noff :+= Array(-1,-1)
+      noff :+= Array(1,-1)
+      noff :+= Array(-1,1)
+      noff :+= Array(1,1)
+    }
+
+    noff
+  }
+
+  override def toString: String = {
+    "Grid!!!"
+  }
+
+  if(height < 1 || width < 1){
+    throw new Exception("Insufficient data provided")
   }
 
 
